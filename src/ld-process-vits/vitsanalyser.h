c442648122090456ce0e68e967b893f2d8b66e4c
Joseph Burns <josalo@josalo.com>
2026-08-04 12:58:21 -0700

    ld-process-vits: reformat to new .clang-format
    Aligns style with decode-orc ahead of upstream contributions.

--- src/ld-process-vits/vitsanalyser.h
+++ src/ld-process-vits/vitsanalyser.h
@@ -1,35 +1,19 @@
-/************************************************************************
-
-    vitsanalyser.h
-
-    ld-process-vits - Vertical Interval Test Signal processing
-    Copyright (C) 2020 Simon Inns
-
-    This file is part of ld-decode-tools.
-
-    ld-process-vits is free software: you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-************************************************************************/
+/*
+ * File:        vitsanalyser.h
+ * Module:      analysis
+ * Purpose:     Per-field white SNR and black PSNR measurement from VITS lines
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-FileCopyrightText: 2020 Simon Inns
+ */
 
 #ifndef VITSANALYSER_H
 #define VITSANALYSER_H
 
-#include <QObject>
 #include <QAtomicInt>
-#include <QThread>
 #include <QDebug>
-
+#include <QObject>
+#include <QThread>
 #include <cmath>
 
 #include "lddecodemetadata.h"
@@ -38,30 +22,33 @@
 class ProcessingPool;
 
 class VitsAnalyser : public QThread {
-    Q_OBJECT
+  Q_OBJECT
 
-public:
-    explicit VitsAnalyser(QAtomicInt& _abort, ProcessingPool& _processingPool, QObject *parent = nullptr);
+ public:
+  explicit VitsAnalyser(QAtomicInt& _abort, ProcessingPool& _processingPool,
+                        QObject* parent = nullptr);
 
-protected:
-    void run() override;
+ protected:
+  void run() override;
 
-private:
-    // Decoder pool
-    QAtomicInt& abort;
-    ProcessingPool& processingPool;
+ private:
+  // Decoder pool
+  QAtomicInt& abort;
+  ProcessingPool& processingPool;
 
-    // Temporary output buffer
-    LdDecodeMetaData::Field outputData;
+  // Temporary output buffer
+  LdDecodeMetaData::Field outputData;
 
-    // Other settings
-    LdDecodeMetaData::VideoParameters videoParameters;
+  // Other settings
+  LdDecodeMetaData::VideoParameters videoParameters;
 
-    QVector<double> getFieldLineSlice(const SourceVideo::Data &sourceField, qint32 fieldLine, qint32 startUs, qint32 lengthUs);
-    double calculateSnr(QVector<double> &data, bool usePsnr);
-    double calcMean(QVector<double> &data);
-    double calcStd(QVector<double> &data);
-    double roundDouble(double in, qint32 decimalPlaces);
+  QVector<double> getFieldLineSlice(const SourceVideo::Data& sourceField,
+                                    qint32 fieldLine, qint32 startUs,
+                                    qint32 lengthUs);
+  double calculateSnr(QVector<double>& data, bool usePsnr);
+  double calcMean(QVector<double>& data);
+  double calcStd(QVector<double>& data);
+  double roundDouble(double in, qint32 decimalPlaces);
 };
 
-#endif // VITSANALYSER_H
+#endif  // VITSANALYSER_H
