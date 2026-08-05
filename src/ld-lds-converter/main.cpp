057bc0c45bf04e4377a35575529e2f1de06fc7b5
Joseph Burns <josalo@josalo.com>
2026-08-04 18:49:06 -0700

    ld-lds-converter: reformat to new .clang-format
    Aligns style with decode-orc ahead of upstream contributions. The
    licence banner becomes the orc File/Module/Purpose block; every existing
    copyright holder is carried across verbatim as an SPDX-FileCopyrightText
    line. No semantic change: token streams are identical to the previous
    revision once comments, whitespace, include order, string-literal splits
    and preprocessor line continuations are normalised.

--- src/ld-lds-converter/main.cpp
+++ src/ld-lds-converter/main.cpp
@@ -1,131 +1,128 @@
-/************************************************************************
+/*
+ * File:        main.cpp
+ * Module:      cli
+ * Purpose:     Command-line entry point for the ld-lds-converter tool
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-FileCopyrightText: 2019-2020 Simon Inns
+ */
 
-    main.cpp
-
-    ld-lds-converter - 10-bit to 16-bit .lds converter for ld-decode
-    Copyright (C) 2019-2020 Simon Inns
-
-    This file is part of ld-decode-tools.
-
-    ld-lds-converter is free software: you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-************************************************************************/
-
+#include <QCommandLineParser>
 #include <QCoreApplication>
 #include <QDebug>
 #include <QtGlobal>
-#include <QCommandLineParser>
 
-#include "tbc/logging.h"
 #include "dataconverter.h"
+#include "tbc/logging.h"
 
-int main(int argc, char *argv[])
-{
-    // Set 'binary mode' for stdin and stdout on Windows
-    setBinaryMode();
+int main(int argc, char *argv[]) {
+  // Set 'binary mode' for stdin and stdout on Windows
+  setBinaryMode();
 
-    // Install the local debug message handler
-    setDebug(true);
-    qInstallMessageHandler(debugOutputHandler);
+  // Install the local debug message handler
+  setDebug(true);
+  qInstallMessageHandler(debugOutputHandler);
 
-    QCoreApplication a(argc, argv);
+  QCoreApplication a(argc, argv);
 
-    // Set application name and version
-    QCoreApplication::setApplicationName("ld-lds-converter");
-    QCoreApplication::setApplicationVersion(QString("ld-decode-tools - Branch: %1 / Commit: %2").arg(APP_BRANCH, APP_COMMIT));
-    QCoreApplication::setOrganizationDomain("domesday86.com");
+  // Set application name and version
+  QCoreApplication::setApplicationName("ld-lds-converter");
+  QCoreApplication::setApplicationVersion(
+      QString("ld-decode-tools - Branch: %1 / Commit: %2")
+          .arg(APP_BRANCH, APP_COMMIT));
+  QCoreApplication::setOrganizationDomain("domesday86.com");
 
-    // Set up the command line parser
-    QCommandLineParser parser;
-    parser.setApplicationDescription(
-                "ld-lds-converter - 10-bit to 16-bit .lds converter for ld-decode\n"
-                "\n"
-                "(c)2018-2020 Simon Inns\n"
-                "GPLv3 Open-Source - github: https://github.com/happycube/ld-decode");
-    parser.addHelpOption();
-    parser.addVersionOption();
+  // Set up the command line parser
+  QCommandLineParser parser;
+  parser.setApplicationDescription(
+      "ld-lds-converter - 10-bit to 16-bit .lds converter for ld-decode\n"
+      "\n"
+      "(c)2018-2020 Simon Inns\n"
+      "GPLv3 Open-Source - github: https://github.com/happycube/ld-decode");
+  parser.addHelpOption();
+  parser.addVersionOption();
 
-    // Add the standard debug options --debug and --quiet
-    addStandardDebugOptions(parser);
+  // Add the standard debug options --debug and --quiet
+  addStandardDebugOptions(parser);
 
-    // Option to specify input video file (-i)
-    QCommandLineOption sourceVideoFileOption(QStringList() << "i" << "input",
-                QCoreApplication::translate("main", "Specify input laserdisc sample file (default is stdin)"),
-                QCoreApplication::translate("main", "file"));
-    parser.addOption(sourceVideoFileOption);
+  // Option to specify input video file (-i)
+  QCommandLineOption sourceVideoFileOption(
+      QStringList() << "i" << "input",
+      QCoreApplication::translate(
+          "main", "Specify input laserdisc sample file (default is stdin)"),
+      QCoreApplication::translate("main", "file"));
+  parser.addOption(sourceVideoFileOption);
 
-    // Option to specify output video file (-o)
-    QCommandLineOption targetVideoFileOption(QStringList() << "o" << "output",
-                QCoreApplication::translate("main", "Specify output laserdisc sample file (default is stdout)"),
-                QCoreApplication::translate("main", "file"));
-    parser.addOption(targetVideoFileOption);
+  // Option to specify output video file (-o)
+  QCommandLineOption targetVideoFileOption(
+      QStringList() << "o" << "output",
+      QCoreApplication::translate(
+          "main", "Specify output laserdisc sample file (default is stdout)"),
+      QCoreApplication::translate("main", "file"));
+  parser.addOption(targetVideoFileOption);
 
-    // Option to unpack 10-bit data (-u)
-    QCommandLineOption showUnpackOption(QStringList() << "u" << "unpack",
-                                       QCoreApplication::translate("main", "Unpack 10-bit data into 16-bit (default)"));
-    parser.addOption(showUnpackOption);
+  // Option to unpack 10-bit data (-u)
+  QCommandLineOption showUnpackOption(
+      QStringList() << "u" << "unpack",
+      QCoreApplication::translate("main",
+                                  "Unpack 10-bit data into 16-bit (default)"));
+  parser.addOption(showUnpackOption);
 
-    // Option to pack 16-bit data (-p)
-    QCommandLineOption showPackOption(QStringList() << "p" << "pack",
-                                       QCoreApplication::translate("main", "Pack 16-bit data into 10-bit"));
-    parser.addOption(showPackOption);
+  // Option to pack 16-bit data (-p)
+  QCommandLineOption showPackOption(
+      QStringList() << "p" << "pack",
+      QCoreApplication::translate("main", "Pack 16-bit data into 10-bit"));
+  parser.addOption(showPackOption);
 
-    // Option to unpack 10-bit data with RIFF WAV headers (-r)
-    QCommandLineOption showRIFFOption(QStringList() << "r" << "riff",
-                                        QCoreApplication::translate("main", "Unpack 10-bit data into 16-bit with RIFF WAV headers (use this ONLY for FlaCCL)"));
-    parser.addOption(showRIFFOption);
+  // Option to unpack 10-bit data with RIFF WAV headers (-r)
+  QCommandLineOption showRIFFOption(
+      QStringList() << "r" << "riff",
+      QCoreApplication::translate("main",
+                                  "Unpack 10-bit data into 16-bit with RIFF "
+                                  "WAV headers (use this ONLY for FlaCCL)"));
+  parser.addOption(showRIFFOption);
 
-    // Process the command line arguments given by the user
-    parser.process(a);
+  // Process the command line arguments given by the user
+  parser.process(a);
 
-    // Standard logging options
-    processStandardDebugOptions(parser);
-    emitDeprecatedToolWarning();
+  // Standard logging options
+  processStandardDebugOptions(parser);
+  emitDeprecatedToolWarning();
 
-    // Get the configured settings from the parser
-    bool isUnpacking = parser.isSet(showUnpackOption);
-    bool isPacking = parser.isSet(showPackOption);
-    bool isRIFF = parser.isSet(showRIFFOption);
-    QString inputFileName = parser.value(sourceVideoFileOption);
-    QString outputFileName = parser.value(targetVideoFileOption);
+  // Get the configured settings from the parser
+  bool isUnpacking = parser.isSet(showUnpackOption);
+  bool isPacking = parser.isSet(showPackOption);
+  bool isRIFF = parser.isSet(showRIFFOption);
+  QString inputFileName = parser.value(sourceVideoFileOption);
+  QString outputFileName = parser.value(targetVideoFileOption);
 
-    bool modeUnpack = true;
-    if (isPacking) modeUnpack = false;
+  bool modeUnpack = true;
+  if (isPacking) modeUnpack = false;
 
-    bool modeRIFF = false;
-    if (isRIFF) modeRIFF = true;
+  bool modeRIFF = false;
+  if (isRIFF) modeRIFF = true;
 
-    // Check that both pack and unpack are not set
-    if (isUnpacking && isPacking) {
-        // Quit with error
-        qCritical("Specify only --unpack (-u) or --pack (-p) - not both!");
-        return -1;
-    }
+  // Check that both pack and unpack are not set
+  if (isUnpacking && isPacking) {
+    // Quit with error
+    qCritical("Specify only --unpack (-u) or --pack (-p) - not both!");
+    return -1;
+  }
 
-    // Check that RIFF option is not set with pack (only unpack)
-    if (isRIFF && ! isUnpacking) {
-        // Quit with error
-        qCritical("You can only write RIFF headers with --unpack (-u)");
-        return -1;
-    }
+  // Check that RIFF option is not set with pack (only unpack)
+  if (isRIFF && !isUnpacking) {
+    // Quit with error
+    qCritical("You can only write RIFF headers with --unpack (-u)");
+    return -1;
+  }
 
-    // Initialise the data conversion object
-    DataConverter dataConverter(inputFileName, outputFileName, !modeUnpack, modeRIFF);
+  // Initialise the data conversion object
+  DataConverter dataConverter(inputFileName, outputFileName, !modeUnpack,
+                              modeRIFF);
 
-    // Process the data conversion
-    dataConverter.process();
+  // Process the data conversion
+  dataConverter.process();
 
-    // Quit with success
-    return 0;
+  // Quit with success
+  return 0;
 }
