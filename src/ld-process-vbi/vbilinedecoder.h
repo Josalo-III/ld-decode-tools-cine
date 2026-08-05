d12dc37a949006d212d3ac054c90075a52aa9058
Joseph Burns <josalo@josalo.com>
2026-08-04 18:49:06 -0700

    ld-process-vbi: reformat to new .clang-format
    Aligns style with decode-orc ahead of upstream contributions. The
    licence banner becomes the orc File/Module/Purpose block; every existing
    copyright holder is carried across verbatim as an SPDX-FileCopyrightText
    line. No semantic change: token streams are identical to the previous
    revision once comments, whitespace, include order, string-literal splits
    and preprocessor line continuations are normalised.

--- src/ld-process-vbi/vbilinedecoder.h
+++ src/ld-process-vbi/vbilinedecoder.h
@@ -1,34 +1,19 @@
-/************************************************************************
-
-    vbilinedecoder.h
-
-    ld-process-vbi - VBI and IEC NTSC specific processor for ld-decode
-    Copyright (C) 2018-2019 Simon Inns
-
-    This file is part of ld-decode-tools.
-
-    ld-process-vbi is free software: you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-************************************************************************/
+/*
+ * File:        vbilinedecoder.h
+ * Module:      vbi
+ * Purpose:     Per-field VBI line decode worker
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-FileCopyrightText: 2018-2019 Simon Inns
+ */
 
 #ifndef VBILINEDECODER_H
 #define VBILINEDECODER_H
 
-#include <QObject>
 #include <QAtomicInt>
-#include <QThread>
 #include <QDebug>
+#include <QObject>
+#include <QThread>
 
 #include "lddecodemetadata.h"
 #include "sourcevideo.h"
@@ -36,25 +21,27 @@
 class DecoderPool;
 
 class VbiLineDecoder : public QThread {
-    Q_OBJECT
+  Q_OBJECT
 
-public:
-    explicit VbiLineDecoder(QAtomicInt& _abort, DecoderPool& _decoderPool, QObject *parent = nullptr);
+ public:
+  explicit VbiLineDecoder(QAtomicInt& _abort, DecoderPool& _decoderPool,
+                          QObject* parent = nullptr);
 
-    // The range of field lines needed from the input file (1-based, inclusive)
-    static constexpr qint32 startFieldLine = 6;
-    static constexpr qint32 endFieldLine = 22;
+  // The range of field lines needed from the input file (1-based, inclusive)
+  static constexpr qint32 startFieldLine = 6;
+  static constexpr qint32 endFieldLine = 22;
 
-protected:
-    void run() override;
+ protected:
+  void run() override;
 
-private:
-    // Decoder pool
-    QAtomicInt& abort;
-    DecoderPool& decoderPool;
+ private:
+  // Decoder pool
+  QAtomicInt& abort;
+  DecoderPool& decoderPool;
 
-    SourceVideo::Data getFieldLine(const SourceVideo::Data& sourceField, qint32 fieldLine,
-                                   const LdDecodeMetaData::VideoParameters& videoParameters);
+  SourceVideo::Data getFieldLine(
+      const SourceVideo::Data& sourceField, qint32 fieldLine,
+      const LdDecodeMetaData::VideoParameters& videoParameters);
 };
 
-#endif // VBILINEDECODER_H
+#endif  // VBILINEDECODER_H
